fd7c392464fe0f84ac3c70d6a5146805115da203
Gustavo Rezende <38204615+Jairzaoo@users.noreply.github.com>
2025-04-11 18:22:43 -0300

    Update admin.py

--- EclesIPay/dizimo/admin.py
+++ EclesIPay/dizimo/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Usuario, Paroquia
+from .models import Usuario, Paroquia, EmailLog
 
 class CustomUserAdmin(UserAdmin):
     model = Usuario
@@ -32,8 +32,25 @@ class ParoquiaAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return request.user.is_superuser
 
+class EmailLogAdmin(admin.ModelAdmin):
+    list_display = ['user', 'email_type', 'sent_at', 'successful']
+    list_filter = ['email_type', 'sent_at', 'successful']
+    search_fields = ['user__email', 'subject']
+    readonly_fields = ['user', 'email_type', 'sent_at', 'subject', 'successful', 'error_message']
+    date_hierarchy = 'sent_at'
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
 admin.site.register(Paroquia, ParoquiaAdmin)
 admin.site.register(Usuario, CustomUserAdmin)
+admin.site.register(EmailLog, EmailLogAdmin)
 admin.site.site_header = "EclesIPay Administração"
 admin.site.site_title = "Portal EclesIPay"
 admin.site.index_title = "Bem-vindo ao Portal EclesIPay"
